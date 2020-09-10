c1b25ef62e0010ecb71250acaabf709a243295d8
mengjian <jmeng15@asu.edu>
2020-09-09 22:30:31 -0700

    Updated

--- models/resnet18_imagenet_quant.py
+++ models/resnet18_imagenet_quant.py
@@ -1,18 +1,8 @@
 import torch.nn as nn
 import math
-import torch.utils.model_zoo as model_zoo
 from .quant import ClippedReLU, int_conv2d, int_linear
 
 __all__ = ["resnet18_imagenet_quant"]
-
-
-model_urls = {
-    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
-    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
-    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
-    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
-}
 
 
 def conv3x3(in_planes, out_planes, stride=1):
